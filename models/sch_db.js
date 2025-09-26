4de0feb11b93ce3e3ad22dab57d402e79450eeb3
MahmoudMohamedAli <mahmoudalielkout@gmail.com>
2025-09-26 16:16:19 +0200

    use Geojson lib

--- models/sch_db.js
+++ models/sch_db.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
+
 const sch_db = new Schema({
     name: {
         type: String,
@@ -14,8 +27,10 @@ const sch_db = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
+    },
+    geometry: GeoSchema
 });
+
 
 const Sch_db = mongoose.model("sch_db", sch_db);
 module.exports = Sch_db;
--- routes/app.js
+++ routes/app.js
@@ -3,8 +3,33 @@ const router = express.Router();
 const Sch_db = require("../models/sch_db");
 
 router.get("/test", (req, res, next) => {
-    res.send({METHOD: "Get"});
+
+       // Parse latitude and longitude from the request query
+    const lng = parseFloat(req.query.lng);
+    const lat = parseFloat(req.query.lat);
+
+    // Ensure coordinates are valid before proceeding
+    if (isNaN(lng) || isNaN(lat)) {
+        return res.status(400).send({ err: "Invalid longitude or latitude provided." });
+    }
+    Sch_db.aggregate([
+        {
+            $geoNear: {
+                near: {
+                    type: "Point",
+                    coordinates: [lng, lat]
+                },
+                distanceField: "dist.calculated", // A new field that will contain the distance
+                spherical: true,
+                maxDistance: 100000 // The maximum distance in meters (100 kilometers)
+            }
+        }
+    ]).then(function(results) {
+        // Send the results back to the client
+        res.send(results);
+    }).catch(next); // Pass any errors to the Express error handler
 });
+
 
 router.post("/test", (req, res, next) => {
    // console.log(req.body);
